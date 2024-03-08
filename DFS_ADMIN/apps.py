f5bbd5caf335fb11b84f00aca049c3207794e8da
Jeremy Larsen <larsenj2064@gmail.com>
2024-03-08 10:56:37 -0800

    Admin App Name Correction

--- DFS_ADMIN/apps.py
+++ DFS_ADMIN/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class AdminSoftDashboardConfig(AppConfig):
+class DFS_ADMINDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_soft'
+    name = 'DFS_ADMIN'
     icon = 'fa fa-user'
--- DFS_ADMIN/urls.py
+++ DFS_ADMIN/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from admin_soft import views
+from DFS_ADMIN import views
 from django.contrib.auth import views as auth_views
 
 
