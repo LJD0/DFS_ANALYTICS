eeccbcdd956d235cf83bbb8e6b27142de2cf078a
Jeremy Larsen <larsenj2064@gmail.com>
2024-03-08 17:37:11 -0800

    minor tweaks

--- DFS_PAGES/models.py
+++ DFS_PAGES/models.py
@@ -127,13 +127,11 @@ class Features(models.Model):
 
 class OurTeam(models.Model):
     name = "Our Team"
-    first_name = models.CharField(max_length=100)
-    title = models.TextField()
+    full_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='')
-    twitter_link = models.CharField(max_length=200, blank=True)
-    facebook_link = models.CharField(max_length=200, blank=True)
-    instagram_link = models.CharField(max_length=200, blank=True)
-
+    linkedin_link = models.CharField(max_length=200, blank=True)
+    blurb = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
         return self.name
